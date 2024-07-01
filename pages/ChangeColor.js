8d52aad6c8a5b7dfff9b240f728060ec6067ff46
ceydaaltun <ceydadotcom@gmail.com>
2024-07-01 14:45:21 +0300

    deploy-error

--- pages/ChangeColor.js
+++ pages/ChangeColor.js
@@ -1,19 +1,23 @@
 import { useEffect, useState } from "react";
 
-
 export default function ChangeColor({ children }) {
     const [color, setColor] = useState("#333454");
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
 
     const handleColorChange = () => {
         const newColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
-        setColor(newColor)
+        setColor(newColor);
     };
 
     useEffect(() => {
-        document.body.style.backgroundColor = color;
-    }, [color]);
+        if (isClient) {
+            document.body.style.backgroundColor = color;
+        }
+    }, [color, isClient]);
 
-
-    return children({ color, handleColorChange});
-
-};
+    return typeof children === 'function' ? children({ color, handleColorChange }) : null;
+}
